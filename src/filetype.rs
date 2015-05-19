7370f99914216d6073ec31370b2ac10a10f17072
Cliff L. Biffle <code@cliffle.com>
2015-05-18 17:32:11 -0700

    Accept content-type overrides from the environment.
    This mirrors one of my favorite Publicfile patches.  Unlike that patch,
    this explicitly allows the administrator to override the canned type
    mapping.

--- src/filetype.rs
+++ src/filetype.rs
@@ -1,15 +1,22 @@
-pub fn filetype(file_path: &[u8]) -> &'static [u8] {
+use std::env;
+use std::ffi::OsString;
+
+use std::os::unix::ffi::OsStringExt;
+use std::iter::FromIterator;
+
+pub fn filetype(file_path: &[u8]) -> Vec<u8> {
   match file_path.rsplitn(2, |b| *b == b'.').next() {
-    Some(ext) => canned_mapping(ext),
+    Some(ext) => env_mapping(ext)
+                   .unwrap_or_else(|| canned_mapping(ext)),
 
     // TODO: a path without an extension should perhaps not be served as
     // text/plain?
-    _ => b"text/plain",
+    _ => b"text/plain".iter().cloned().collect(),
   }
 }
 
-fn canned_mapping(ext: &[u8]) -> &'static [u8] {
-  match ext {
+fn canned_mapping(ext: &[u8]) -> Vec<u8> {
+  let mimetype: &[u8] = match ext {
     b"html"           => b"text/html",
     b"gif"            => b"image/gif",
     b"jpeg" | b"jpg"  => b"image/jpeg",
@@ -17,5 +24,14 @@ fn canned_mapping(ext: &[u8]) -> &'static [u8] {
     b"pdf"            => b"application/pdf",
     b"css"            => b"text/css",
     _ => b"text/plain",
-  }
+  };
+  mimetype.iter().cloned().collect()
+}
+
+fn env_mapping(ext: &[u8]) -> Option<Vec<u8>> {
+  let key = Vec::from_iter(
+      b"CT_".iter()
+      .chain(ext.iter())
+      .cloned());
+  env::var_os(OsString::from_vec(key)).map(|s| s.into_vec())
 }
