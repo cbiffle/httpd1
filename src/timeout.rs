7db129734d068bd3eba1a73f8db99f7b9dd81529
Cliff L. Biffle <code@cliffle.com>
2020-01-18 18:13:13 -0800

    Rewrite write-with-timeout in safe Rust.

--- src/timeout.rs
+++ src/timeout.rs
@@ -6,8 +6,6 @@ use std::io;
 use std::os::unix::io::AsRawFd;
 
 mod ffi {
-    use std::mem::MaybeUninit;
-    use std::ptr::null_mut;
     use libc::{c_int, time_t};
 
     pub fn wait_for_data(fd: c_int, seconds: time_t) -> nix::Result<()> {
@@ -35,25 +33,29 @@ mod ffi {
         Ok(())
     }
 
-    pub unsafe fn wait_for_writeable(fd: c_int, seconds: time_t) -> c_int {
-        let mut tv = libc::timeval {
+    pub fn wait_for_writeable(fd: c_int, seconds: time_t) -> nix::Result<()> {
+        use nix::sys::select::{select, FdSet};
+        use nix::sys::time::TimeVal;
+
+        let mut tv = TimeVal::from(libc::timeval {
             tv_sec: seconds,
             tv_usec: 0,
-        };
+        });
 
-        let mut fds = MaybeUninit::uninit();
-        libc::FD_ZERO(fds.as_mut_ptr());
-        let fds = fds.as_mut_ptr();
-        libc::FD_SET(fd, fds);
+        let mut fds = FdSet::new();
+        fds.insert(fd);
 
-        if libc::select(fd + 1, null_mut(), fds, null_mut(), &mut tv) == -1 {
-            return -1;
+        select(
+            fd + 1,
+            None,
+            Some(&mut fds),
+            None,
+            Some(&mut tv),
+        )?;
+        if !fds.contains(fd) {
+            return Err(nix::errno::Errno::ETIMEDOUT.into());
         }
-        if !libc::FD_ISSET(fd, fds) {
-            return -1;
-        }
-
-        0
+        Ok(())
     }
 }
 
@@ -98,15 +100,8 @@ where
 
 impl WriteTimeout for fs::File {
     fn wait_for_writeable(&mut self, seconds: u32) -> io::Result<()> {
-        let r = unsafe {
-            ffi::wait_for_writeable(self.as_raw_fd(), seconds as time_t)
-        };
-
-        if r == 0 {
-            Ok(())
-        } else {
-            Err(io::Error::last_os_error())
-        }
+        ffi::wait_for_writeable(self.as_raw_fd(), seconds as time_t)
+            .map_err(cvt_err)
     }
 }
 
